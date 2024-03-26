63dc60bab7da09d64553e438e7486f3e8b49cd0c
arsh2026 <125494350+arsh2026@users.noreply.github.com>
2024-03-26 13:34:42 +0530

    Add files via upload

---
+++ hackathon/script.js
@@ -0,0 +1,14 @@
+// let whereBox=document.querySelector(".place");
+// let HowManyBox=document.querySelector(".amount");
+// let aTime=document.querySelector(".coming");
+// let lTime=document.querySelector(".going");
+
+
+// function procedure(){
+//     if (whereBox.value===""|| HowManyBox.value==="" || aTime.value==="" || lTime.value===""){
+//         alert("Please Fill all the blanks");
+//     }
+//     else{
+//         alert("Booking Done");
+// }
+// }
